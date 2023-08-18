e21a3ddbd77624b6f39fc36b4f7f95c4da758600
George Samuels <dominic.g.samuels@outlook.com>
2023-08-18 11:04:31 +0100

    Tidying API hosting code

--- db/connection.js
+++ db/connection.js
@@ -10,12 +10,11 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
 }
 
 
-const config = {};
+const config = {
+  connectionString: process.env.DATABASE_URL,
+  max: 2, 
+};
 
-if (ENV === 'production') {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
-}
-new Pool(config) 
+const pool = new Pool(config) 
 
 module.exports = new Pool();
