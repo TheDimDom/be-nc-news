ea91fa7ca34d087d20799d7d7f3c0bb1dc933d03
George Samuels <dominic.g.samuels@outlook.com>
2023-08-18 11:01:24 +0100

    api

--- db/connection.js
+++ db/connection.js
@@ -8,11 +8,14 @@ require('dotenv').config({
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
+
+
 const config = {};
 
 if (ENV === 'production') {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
+new Pool(config) 
 
 module.exports = new Pool();
